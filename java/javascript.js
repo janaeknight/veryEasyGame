a715d6afcddf8f23a1b0e8d9b1d2ac2db44ef4b1
Knight <janae.jpeg@gmail.com>
2020-01-28 11:18:56 -0600

    unlocked mortal kombat character update

---
+++ java/javascript.js
@@ -0,0 +1,55 @@
+var Q = [
+    "What does the fox say? Nobody Nose",
+    "Worst dissappointment - hoveboards or apple watch?",
+    "Who is this? Obama.",
+    "What Carly is this? Carly Rae Jepsen/Carly Shay",
+    "What song video is this shot from? - Cups, Anna Kendrick.",
+    "What's that song that goes dunDunDUnDun daDunDUnDUnDDUnDUNDunDUND MEROW duNDUnDUNDuNDuNDUN nuNunuNuNAtcha?",
+    "(Finish this) Walk into the store like - Whaddup I got a big (🐓) - Thrift Store",
+    "What famous 🍑 is this? (Harley Quinn/Margot Robbie) Jennifer Anison, Kim K, etc",
+    "Name a better Duo- (pic of zach and cody(left) Raven/Girl - Drake and Josh - Zanessa (Zac Efron and Vanessa Hudgens), Phineas & Ferb",
+    "What's 9+10? (21)",
+    "What is this (club penguin pet)?",
+    "Who is this? (Slenderman, <i>The</i> Slenderman, Skinny White Guy, Mouthface.",
+    "Ani-bandz",
+    "Purple Palace/Putt Putt",
+    "What is this called? Meme rage face?"
+]
+
+var A = [
+    //checkDataAttr
+]
+var userA = [
+
+]
+
+var ohBrother = document.getElementById("soundOhBrother");
+
+function playOhBrother() {
+    ohBrother.play();
+} function pauseOhBrother() {
+    ohBrother.pause();
+}
+
+$("a").click(function() {
+    alert( "Handler for .click() called." );
+});
+
+
+// var divs = document.getElementsByTagName("div");
+// for(var i = 0; i < divs.length; i++){
+//    //do something to each div like
+//    divs[i].innerHTML = "something new...";
+// }
+
+
+
+
+
+
+
+
+function startgame() {
+    $("#start").hide();
+    $("#stages").show();
+}
