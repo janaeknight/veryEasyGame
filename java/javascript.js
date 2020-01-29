a6d6d28a1f8fbd38a6996abcd3db44a95fb153b0
Knight <janae.jpeg@gmail.com>
2020-01-28 22:52:43 -0600

    jukebox money update

--- java/javascript.js
+++ java/javascript.js
@@ -12,12 +12,9 @@ var Q = [
     "What does the fox say? Nobody Nose",
     "Worst dissappointment - hoveboards or apple watch?",
     "Who is this? Obama.",
-    "What Carly is this? Carly Rae Jepsen/Carly Shay",
     "What song video is this shot from? - Cups, Anna Kendrick.",
     "What's that song that goes dunDunDUnDun daDunDUnDUnDDUnDUNDunDUND MEROW duNDUnDUNDuNDuNDUN nuNunuNuNAtcha?",
-    "(Finish this) Walk into the store like - Whaddup I got a big (🐓) - Thrift Store",
     "What famous 🍑 is this? (Harley Quinn/Margot Robbie) Jennifer Anison, Kim K, etc",
-    "What's 9+10? (21)",
     "What is this (club penguin pet)?",
     "Who is this? (Slenderman, <i>The</i> Slenderman, Skinny White Guy, Mouthface.",
     "Ani-bandz",
@@ -27,6 +24,9 @@ var Q = [
 
 var A = [
     "104", // e. None of the above
+    "202", // 21,
+    "401", // Carly Shay
+    "500" // 🐓
 ]
 
 
@@ -40,13 +40,6 @@ $("a").click(function() {
     thisStage.hide();
     nextStage.show();
 });
-
-
-// var divs = document.getElementsByTagName("div");
-// for(var i = 0; i < divs.length; i++){
-//    //do something to each div like
-//    divs[i].innerHTML = "something new...";
-// }
 
 
 
