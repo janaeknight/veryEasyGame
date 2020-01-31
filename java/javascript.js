c8ba8cc0af13109f3d97916b8b8bc2ee46c0630b
Knight <janae.jpeg@gmail.com>
2020-01-30 18:24:07 -0600

    prickly pear cactus update

--- java/javascript.js
+++ java/javascript.js
@@ -18,15 +18,16 @@ var Q = [
 ]
 
 var A = [
-    "104", // e. None of the above
-    "202", // 21,
-    "300", // an oxymoron
-    "401", // Carly Shay
-    "500", // 🐓
-    "601", // First of the Year (Equinox)
-    "701", // Puffles
-    "802", // Barack Obama
-    "900", // Slenderman
+    104, // None of the above
+    202, // 21,
+    300, // an oxymoron
+    401, // Carly Shay
+    500, // 🐓
+    601, // First of the Year (Equinox)
+    701, // Puffles
+    802, // Barack Obama
+    900, // Slenderman
+    1010, // Will always be a mystery
     "", // 
     "" // 
 ]
@@ -34,10 +35,8 @@ var A = [
 
 
 function startgame() {
-    $("#start").hide();
-    $("#stages").show();
-    $(".stage").hide();
-    $("#set1").show();
+    $("#start").hide(); $("#stages").show();
+    $(".stage").hide(); $("#set1").show();
     //$("#set10").show();
 }
 
@@ -45,19 +44,24 @@ $("a").click(function() {
     var userA = $(this).data("id") // will return the data-id string
     console.log(userA);
 
+        // Updates Score
+        var scoreNum = $("#scoreNum").text();
+        var nextScoreNum = "";
+            if ($.inArray(userA, A) !=-1) {
+                nextScoreNum = parseInt(scoreNum) + 1;
+            } else {
+                nextScoreNum = parseInt(scoreNum) + 0;
+            }
+        $("#scoreNum").text(nextScoreNum);
+
+        // Updates Question # 
+            var currentValue = $("#questionNum").text();
+            var nextQuestionNum = parseInt(currentValue) + 1;
+            $("#questionNum").text(nextQuestionNum);
+
     var thisStage = $(this).closest('.stage');
     var nextStage = $(thisStage).next('.stage');
 
     thisStage.hide();
     nextStage.show();
-
-    updateGame();
 });
-
-
-var questionNum = $("#questionNum").text;
-
-function updateGame() {
-    questionNum+1;
-    console.log(questionNum);
-}
